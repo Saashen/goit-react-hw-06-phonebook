c5fcf6a37b359c583b7358b2f6c75c97cc940b81
Saashen <saashen.kyiv@gmail.com>
2020-02-24 19:51:49 +0200

    Updates

---
+++ precache-manifest.0f5df8759dd1ec8de9e34562721034fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3371e5c16a4f4c0f87bcf8db4baf9af",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "d4a96ceebefd9b421687",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.a8f328b7.chunk.css"
+  },
+  {
+    "revision": "2af8da65c313ee4507c6",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.cb79ff7c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.cb79ff7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4a96ceebefd9b421687",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.f63dc6f7.chunk.js"
+  },
+  {
+    "revision": "9719fa00745b8fafea01",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.0c92875d.js"
+  }
+]);
