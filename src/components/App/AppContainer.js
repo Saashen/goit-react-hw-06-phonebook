a9bf2a86a94b3f07d0609485b41fb550fda6aa13
Saashen <saashen.kyiv@gmail.com>
2020-03-01 21:16:41 +0200

    revert

--- src/components/App/AppContainer.js
+++ src/components/App/AppContainer.js
@@ -2,9 +2,12 @@ import { connect } from 'react-redux';
 import App from './App';
 import * as pbActions from '../../redux/phonebook/phonebookActions';
 
-const mapStateToProps = state => ({
-  contacts: state.contacts,
-});
+const mapStateToProps = state => {
+  console.log(state.contacts);
+  return {
+    contacts: state.contacts,
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   getContactFromLS: contacts => dispatch(pbActions.getContactFromLS(contacts)),
